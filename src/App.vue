3e16af572cdd87dc10bf4cb54a042e68d05741a8
Renato Araga <admin@bkprecisioninstruments.com>
2019-05-09 11:20:11 -0700

    Added axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos
       v-bind:todos="todos"
       v-on:del-todo="deleteTodo"
@@ -10,10 +10,10 @@
 </template>
 
 <script>
-
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import Header from './components/layout/Header';
+import axios from 'axios';
 
 export default {
   name: 'app',
@@ -24,41 +24,47 @@ export default {
  },
  data () {
    return {
-   todos: [
-     {
-       id: 1,
-       title: "Todo One",
-       completed: false,
-     },
-     {
-       id: 2,
-       title: "Todo Two",
-       completed: true,
-     },
-     {
-       id: 3,
-       title: "Todo Three",
-       completed: false
-     },
-   ]
+    todos: []
   }
  },
  methods: {
-   deleteTodo(id) {
+  deleteTodo(id) {
      this.todos = this.todos.filter(todo => todo.id !== id);
-   }
+  },
+  addTodo(newTodo) {
+    this.todos = [...this.todos, newTodo];
+  }
+ },
+ // Think about created() as similar to ComponentDidMount().
+ created() {
+   axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(res => this.todos = res.data)
+    .catch (err => console.log(err));
  }
 }
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
+
+
 </style>
